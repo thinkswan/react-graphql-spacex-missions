6df8743172946f9a8a697c07a8c44b1649159001
Graham Swan <thinkswan@gmail.com>
2019-02-23 21:42:05 +0100

    Add Apollo dependencies and initial query

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,28 @@
 import React, { Component } from 'react'
+import ApolloClient from 'apollo-boost'
+import { ApolloProvider } from 'react-apollo'
+import Launches from './components/Launches'
 import './App.css'
 import logo from './logo.png'
+
+const client = new ApolloClient({
+  uri: 'http://localhost:5000/graphql'
+})
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <img src={logo} alt="SpaceX" style={{
-          display: 'block',
-          width: 300,
-          margin: 'auto'
-        }} />
-      </div>
+      <ApolloProvider client={client}>
+        <div className="container">
+          <img src={logo} alt="SpaceX" style={{
+            display: 'block',
+            width: 300,
+            margin: 'auto'
+          }} />
+
+          <Launches />
+        </div>
+      </ApolloProvider>
     )
   }
 }
