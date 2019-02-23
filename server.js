9af4079c8017c1c1cc29c977e6a73ffa2edfa00b
Graham Swan <thinkswan@gmail.com>
2019-02-23 21:43:56 +0100

    Add CORS headers to server

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const graphQLHTTP = require('express-graphql')
+const cors = require('cors')
 const schema = require('./schema')
 
+const PORT = process.env.PORT || 5000
 const app = express()
 
-const PORT = process.env.PORT || 5000
+app.use(cors())
 
 app.use('/graphql', graphQLHTTP({
   schema,
