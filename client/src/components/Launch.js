844594fcc6d90adc847c6a52b5a029fbcc8889c7
Graham Swan <thinkswan@gmail.com>
2019-02-24 20:18:33 +0100

    Add classnames and moment; add <MissionKey>

--- client/src/components/Launch.js
+++ client/src/components/Launch.js
@@ -1,12 +1,24 @@
-import React from 'react'
+import React from "react";
+import classNames from "classnames";
 
-export default function Launch ({ launch: { flight_number, mission_name,
-  launch_date_local, launch_success } }) {
+export default function Launch({
+  launch: { flight_number, mission_name, launch_date_local, launch_success }
+}) {
   return (
     <div className="card card-body mb-3">
       <div className="row">
         <div className="col-md-9">
-          <h4>Mission: {mission_name}</h4>
+          <h4>
+            Mission:{" "}
+            <span
+              className={classNames({
+                "text-success": launch_success,
+                "text-danger": !launch_success
+              })}
+            >
+              {mission_name}
+            </span>
+          </h4>
           <p>Date: {launch_date_local}</p>
         </div>
 
@@ -15,5 +27,5 @@ export default function Launch ({ launch: { flight_number, mission_name,
         </div>
       </div>
     </div>
-  )
+  );
 }
--- client/src/components/Launches.js
+++ client/src/components/Launches.js
@@ -1,7 +1,8 @@
-import React, { Component, Fragment } from 'react'
-import gql from 'graphql-tag'
-import { Query } from 'react-apollo'
-import Launch from './Launch'
+import React, { Component, Fragment } from "react";
+import gql from "graphql-tag";
+import { Query } from "react-apollo";
+import Launch from "./Launch";
+import MissionKey from "./MissionKey";
 
 const LAUNCHES_QUERY = gql`
   query LaunchesQuery {
@@ -12,33 +13,31 @@ const LAUNCHES_QUERY = gql`
       launch_success
     }
   }
-`
+`;
 
 export default class Launches extends Component {
-  render () {
+  render() {
     return (
       <Fragment>
-        <h1 className="display-4 my-3">Launches</h1>
+        <h1 className="display-4 my-3">Missions</h1>
+
+        <MissionKey />
 
         <Query query={LAUNCHES_QUERY}>
-          {
-            ({ loading, error, data }) => {
-              if (loading) return <h4>Loading...</h4>
-              if (error) console.log(error)
+          {({ loading, error, data }) => {
+            if (loading) return <h4>Loading...</h4>;
+            if (error) console.log(error);
 
-              return (
-                <Fragment>
-                  {
-                    data.launches.map(launch => (
-                      <Launch key={launch.flight_number} launch={launch} />
-                    ))
-                  }
-                </Fragment>
-              )
-            }
-          }
+            return (
+              <Fragment>
+                {data.launches.map(launch => (
+                  <Launch key={launch.flight_number} launch={launch} />
+                ))}
+              </Fragment>
+            );
+          }}
         </Query>
       </Fragment>
-    )
+    );
   }
 }
