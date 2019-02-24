5d9447627fd3297569812a5b531f6f8eea34d4e9
Graham Swan <thinkswan@gmail.com>
2019-02-24 21:13:57 +0100

    Add <Launch> to display details for individual missions

--- client/src/components/Launch.js
+++ client/src/components/Launch.js
@@ -1,34 +1,96 @@
-import React from "react";
+import React, { Component, Fragment } from "react";
+import gql from "graphql-tag";
+import { Query } from "react-apollo";
+import { Link } from "react-router-dom";
 import classNames from "classnames";
-import Moment from "react-moment";
 
-export default function Launch({
-  launch: { flight_number, mission_name, launch_date_local, launch_success }
-}) {
-  return (
-    <div className="card card-body mb-3">
-      <div className="row">
-        <div className="col-md-9">
-          <h4>
-            Mission:{" "}
-            <span
-              className={classNames({
-                "text-success": launch_success,
-                "text-danger": !launch_success
-              })}
-            >
-              {mission_name}
-            </span>
-          </h4>
-          <p>
-            Date: <Moment format="YYYY-MM-DD HH:mm">{launch_date_local}</Moment>
-          </p>
-        </div>
+const LAUNCH_QUERY = gql`
+  query LaunchQuery($flight_number: Int!) {
+    launch(flight_number: $flight_number) {
+      flight_number
+      mission_name
+      launch_year
+      launch_date_local
+      launch_success
+      rocket {
+        rocket_id
+        rocket_name
+        rocket_type
+      }
+    }
+  }
+`;
 
-        <div className="col-md-3">
-          <button className="btn btn-secondary">Launch details</button>
-        </div>
-      </div>
-    </div>
-  );
+export class Launch extends Component {
+  render() {
+    let { flight_number } = this.props.match.params;
+    flight_number = parseInt(flight_number);
+
+    return (
+      <Fragment>
+        <Query query={LAUNCH_QUERY} variables={{ flight_number }}>
+          {({ loading, error, data }) => {
+            if (loading) return <h4>Loading...</h4>;
+            if (error) console.log(error);
+
+            const {
+              mission_name,
+              flight_number,
+              launch_year,
+              launch_success,
+              rocket: { rocket_id, rocket_name, rocket_type }
+            } = data.launch;
+
+            return (
+              <div>
+                <h1 className="display-4 my-3">
+                  <span className="text-dark">Mission:</span> {mission_name}
+                </h1>
+
+                <h4 className="mb-3">Launch details</h4>
+                <ul className="list-group">
+                  <li className="list-group-item">
+                    Flight number: {flight_number}
+                  </li>
+                  <li className="list-group-item">
+                    Launch year: {launch_year}
+                  </li>
+                  <li className="list-group-item">
+                    Launch successful:{" "}
+                    <span
+                      className={classNames({
+                        "text-success": launch_success,
+                        "text-danger": !launch_success
+                      })}
+                    >
+                      {launch_success ? "Yes" : "No"}
+                    </span>
+                  </li>
+                </ul>
+
+                <h4 className="mb-3">Rocket details</h4>
+                <ul className="list-group">
+                  <li className="list-group-item">Rocket ID: {rocket_id}</li>
+                  <li className="list-group-item">
+                    Rocket name: {rocket_name}
+                  </li>
+                  <li className="list-group-item">
+                    Rocket type: {rocket_type}
+                  </li>
+                </ul>
+
+                <hr />
+
+                <Link to="/" className="btn btn-secondary">
+                  Back
+                </Link>
+              </div>
+            );
+          }}
+        </Query>
+      </Fragment>
+    );
+  }
 }
+
+export default Launch;
